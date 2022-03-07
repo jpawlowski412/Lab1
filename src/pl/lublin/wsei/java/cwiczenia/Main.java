0280d7c9f01a1759d2f77b5b32740237e3341814
Jan Pawłowski <jpawlowski412@gmail.com>
2022-03-07 12:25:39 +0100

    Dodanie wyświetlanie Hello Java World

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -7,6 +7,7 @@ package pl.lublin.wsei.java.cwiczenia;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello Java World\n");
     //przykladowy komentarz jednolinijkowy
     /*
     przykladowy
