8f56f369c42f78eb926df266704eb32aa1889179
Jan Pawłowski <jpawlowski412@gmail.com>
2022-03-07 12:17:44 +0100

    Dodanie komentarzy

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,9 +1,17 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+/**
+ * przykladowy komentarz javadoc
+ */
+
 public class Main {
 
     public static void main(String[] args) {
-
-
+    //przykladowy komentarz jednolinijkowy
+    /*
+    przykladowy
+    komentarz
+    wielolinijkowy
+     */
     }
 }
