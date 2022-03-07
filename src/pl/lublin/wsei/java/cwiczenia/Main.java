89c8c9cdf488e78028e4b32e24a7ad1b4154f6eb
Jan Pawłowski <jpawlowski412@gmail.com>
2022-03-07 13:01:45 +0100

    Nazywaliśmy to "witaminą B3"

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -4,10 +4,6 @@ package pl.lublin.wsei.java.cwiczenia;
 public class Main {
 
     public static void main(String[] args) {
-    int a=3;
-    double b=4.21;
-    String s="jakiś tekst";
-
-    System.out.printf("a=%d, b=%.2f, s=%20s %n",a,b,s);
+    System.out.print("Nazywaliśmy to \"witaminą B3\"");
     }
 }
