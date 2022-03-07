bb0b45dcdfec54d62553b478adf46a70ba258107
Jan Pawłowski <jpawlowski412@gmail.com>
2022-03-07 12:53:09 +0100

    Print Alamakota

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -8,6 +8,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Java World\n");
+        System.out.print("Ala");
+        System.out.print("ma");
+        System.out.print("kota");
     //przykladowy komentarz jednolinijkowy
     /*
     przykladowy
