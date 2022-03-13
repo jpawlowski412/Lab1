a87172a232d5f037186bf8e450f0f25b0d93f1e0
Jan Pawłowski <jpawlowski412@gmail.com>
2022-03-13 11:29:38 +0100

    Pętla Scanner

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -6,14 +6,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    int num1, num2;
-    System.out.print("Podaj pierwszą liczbę: ");
-    num1 = input.nextInt();
-    System.out.print("Podaj drugą liczbę: ");
-    num2 = input.nextInt();
-    System.out.printf("Wynik dodawania %d +%d = %d%n",num1,num2,num1+num2);
-
-
-        }
+        Scanner input = new Scanner(System.in);
+        int num1=0, num2=0;
+        do {
+            System.out.print("Podaj pierwszą liczbę: ");
+            num1 = input.nextInt();
+            if (num1 != 0) {
+                System.out.print("Podaj drugą liczbę: ");
+                num2 = input.nextInt();
+            }
+            System.out.printf("Wynik dodawania %d +%d = %d%n", num1, num2, num1 + num2);
+        } while ((num1 != 0) && (num2 != 0));
+    }
 }
