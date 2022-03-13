d1c3f2fb509fb81886c8d603d75110a3ce944359
Jan Pawłowski <jpawlowski412@gmail.com>
2022-03-13 12:40:13 +0100

    Dodanie własnej klasy my.lib

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Account acc = new Account();
-        acc.setName("Piotr Gołabek");
+        acc.setName("piotr Gołabek");
         System.out.println(acc.getName());
 }
 }
